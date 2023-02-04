3b6556732933cfa6d808d2b32d000ab877ef85f8
Alexander Suslov <sasha_suslov07@mail.ru>
2023-02-04 13:58:54 +0300

    feat: install  swagger

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,17 @@ import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { EnvVariables } from './config/envVariables.enum';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const docsConfig = new DocumentBuilder().setTitle('Good-deeds').build();
+
+  const document = SwaggerModule.createDocument(app, docsConfig);
+  SwaggerModule.setup('docs', app, document);
+
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
 
